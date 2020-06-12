98803d871a24da7a7401f4d193aad6e2561b8046
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-06-12 08:57:39 +0100

    Ring buffer completed - test failing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,25 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.oldest_node = None
+        self.next_oldest = None
+        self.stack = []
 
     def append(self, item):
-        pass
+        # Statement for making first item the oldest
+        if self.oldest_node is None:
+            self.oldest_node = 0
+            return self.stack.append(item)
+        # If list already contains items
+        elif len(self.stack) + 1 <= self.capacity:
+            self.next_oldest = 1
+            return self.stack.append(item)
+
+        else:
+            self.stack[self.oldest_node] = item
+            self.oldest_node += 1 
+            if self.oldest_node + 1  > self.capacity:
+                self.oldest_node = 0
 
     def get(self):
         pass
